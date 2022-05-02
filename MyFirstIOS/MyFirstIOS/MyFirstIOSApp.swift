3f6eddbecf4a89ba8ce67b8b8d733de2c8d2d481
Devsom <moonjipsa@naver.com>
2022-05-02 17:20:37 +0900

    first commit

---
+++ MyFirstIOS/MyFirstIOS/MyFirstIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstIOSApp.swift
+//  MyFirstIOS
+//
+//  Created by 문희재 on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
