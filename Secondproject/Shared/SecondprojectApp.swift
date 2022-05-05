57c74ec0bb09d7e8a548f2862d3e8dbe6169e34b
moonjipsa <moonjipsa@naver.com>
2022-05-05 19:41:14 +0900

    merge branch IMAC & Macbook

---
+++ Secondproject/Shared/SecondprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondprojectApp.swift
+//  Shared
+//
+//  Created by 희재 on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct SecondprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
